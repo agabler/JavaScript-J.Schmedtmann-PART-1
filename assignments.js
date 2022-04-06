be5f2c3b184647a9d51670685df8df601300b2c3
agabler <gora.angelika@icloud.com>
2022-04-06 15:11:06 +0200

    basic knowledge of Java Script

---
+++ assignments.js
@@ -0,0 +1,32 @@
+let country='Poland';
+let continent='Europe';
+let population='6million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(language);
+console.log(population);
+console.log(country);
+
+language= 'polish';
+
+const territory = '20 000m2';
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,54 @@
+
+/*let js = 'amazing'
+if (js === 'amazing') alert('JavaScript is fun');
+
+console.log("Angelika");
+console.log(23);
+
+let firstName = "Angelika";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+let javaScriptIsFun= true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);*/
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageAngelika = now - 1988;
+
+console.log(ageJonas, ageAngelika);
+console.log(ageJonas * 2, ageAngelika / 10, 2 ** 3);
+
+
+const firstName = 'Jonas';
+const lastName = 'Schedtman  n';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
+
+
+
